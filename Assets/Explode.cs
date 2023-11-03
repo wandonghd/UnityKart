3ff841d46ca1ccc792a9cb16aaa2016f51ec320a
wandonghd <wandonghd@gmail.com>
2023-11-02 20:07:01 -0400

    Starting the project

---
+++ Assets/Explode.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explode : MonoBehaviour
+{
+  public GameObject pickupEffect;
+
+  void OnTriggerEnter (Collider other)
+  {
+    if (other.CompareTag("Player"))
+    {
+        Pickup();
+    }
+  }
+
+  void Pickup()
+  {
+    Instantiate(pickupEffect, transform.position, transform.rotation);
+    
+    // Apply effect to the player
+
+    Destroy(gameObject);
+  }
+}
